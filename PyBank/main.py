a29a75940fa6bcf08a068c6fd1ea676c3045e0da
Andrew Neher <andrewneher@gmail.com>
2020-12-28 11:57:39 -0600

    added min/max

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,36 +9,59 @@ pybank_csv = os.path.join("Resources", "budget_data.csv")
 with open(pybank_csv, "r") as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csv_reader = csv.reader(csvfile, delimiter=",")
-    print(csv_reader)
+    #print(csv_reader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csv_reader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
         # Read each row of data after the header
-    for row in csv_reader:
-        print(row[1])
+    #for row in csv_reader:
+        #print(row[1])
 
 
 #Create a bucket for Total Months
-
 bank = open(pybank_csv, "r")
 data = bank.readline()
 print(bank)
 data = bank.readlines()
 total_months = 0
 linecounter = 0
+max_change = 0
 for bank in data:
     linecounter = linecounter + 1
     
-print("The total number of months is", linecounter)
+print("The total number of months is:", linecounter)
 
 for line in data:
     r = line.split(",")
     total_months = total_months + float(r[1])
-print("The total amount is", total_months)
+print("The total amount is:", total_months)
 average = float(total_months / linecounter)
-print("The average change is ", "%.2f" % average)
+print("The average change is: ", "%.2f" % average)
+
+#max_change = 0
+#for line in data:
+#    r = line.split(",")
+#    max_change = max_change + float(r[1])
+#    if max_change <= max_change:
+#        break
+#print("Maximum change is:", max_change)
+
+total = 0
+max_value = 0
+min_value = 0
+
+#Finding Max and Minimum Values
+for line in data:
+    r = line.split(",")
+    value = float(r[1])
+    total = total + value
+    max_value = max(max_value, value)
+    min_value = min(min_value, value)
+print("The maximum change is:", "(" max_value ")")
+print("The minimum change is:", "(" min_value ")")
+
 
 #print(total_months)
 #data['Profit/Losses'].average
@@ -77,10 +100,9 @@ with open(output_path, 'w', newline='') as csvfile:
     csv_writer.writerow(['Total Months:', linecounter])
     csv_writer.writerow(['Total:', total_months])
     csv_writer.writerow(['Average Change:', average])
-    csv_writer.writerow(['Greatest Increase in Profits:', 'Need this figure'])
-    csv_writer.writerow(['Greatest Decrease in Profits:', 'Need this figure'])
+    csv_writer.writerow(['Greatest Increase in Profits:', max_value])
+    csv_writer.writerow(['Greatest Decrease in Profits:', min_value])
 
-print(PyBank_new)
 
 #Define the columns and rows
 #pybank_csv = []
