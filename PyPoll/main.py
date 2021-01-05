b33901cfca43d452359440bc7211a136eb0a39dd
Andrew Neher <andrewneher@gmail.com>
2021-01-05 12:00:00 -0600

    Solved!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,37 +11,32 @@ pypoll_csv = os.path.join("Resources", "election_data.csv")
 voter_id = [0]
 country = [1]
 candidate = [2]
-correy_votes = 0
-tooley_votes = 0
-li_votes = 0
-khan_votes = 0
-khan_percentage = 0
-li_percentage = 0
-tooley_percentage = 0
-correy_percentage = 0
-winner_percentage = 0
+
 winner = str
+
+
+#Outfut Path
+output_path = os.path.join("Output", "PyPoll_new.txt")
+
 
 #Open the CSV
 with open(pypoll_csv, "r") as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csv_reader = csv.reader(csvfile, delimiter=",")
-    #print(csv_reader)
+
 
     # Read the header row first 
     csv_header = next(csv_reader)
-    #print(f"CSV Header: {csv_header}")
 
-        # Read each row of data after the header
-    #for row in csv_reader:
-        #print(row[1])
+
+
 
 #Create a bucket for Total Votes
 poll = open(pypoll_csv, "r")
 data = poll.readline()
 print(poll)
 data = poll.readlines()
-total_votes = 0
+
 votecounter = 0
 for poll in data:
     votecounter = votecounter + 1
@@ -49,15 +44,13 @@ for poll in data:
 #Count toal votes    
 for line in data:
     r = line.split(",")
-    total_votes = total_votes + float(r[0])
-print("Total Votes:", votecounter)
 
+#Print total votes, formatted
+print("  ")
+print("------------------------------------------")
+print("           Total Votes: ", votecounter)
+print("------------------------------------------")
 
-
-
-#Create list of candidates
-myList = ["Khan", "Correy", "O'Tooley", "Li"]
-print(myList)
 
 
 
@@ -68,82 +61,49 @@ with open(pypoll_csv, "r") as csvfile:
 
     # Read the header row first 
     csv_header = next(csv_reader)
-
-    #define column two as candidate vote
-    def pypoll_csv(candidates):
-        candidate = str(candidates[2])
-
+    #Create empty dictionary
+    candidate_votes = {}
     for row in csv_reader:
-        if candidate == 'Khan':
-            khan_votes = float(khan_votes + 1)
-    for row in csv_reader:        
-        if candidate == "Li":
-            li_votes = li_votes + 1
-    for row in csv_reader:        
-        if candidate == "O'Tooley":
-            tooley_votes = tooley_votes + 1
-    for row in csv_reader:        
-        if candidate == "Correy":
-            correy_votes = correy_votes + 1        
+        candidate = row [2]
+        if candidate in candidate_votes.keys():
+            candidate_votes[candidate] +=1
+        else:
+            candidate_votes[candidate] = 1
 
-    print ("Khan recieved: ", khan_votes, "votes", khan_percentage, "%")
-    print ("Li recieved: ", li_votes, "votes", li_percentage, "%")
-    print ("O'Tooley recieved: ", tooley_votes, "votes", tooley_percentage, "%")
-    print ("Correy recieved: ", correy_votes, "votes", correy_percentage, "%")
-    print ("Winner: ", winner)
-    print ("by: ", winner_percentage, "percentage points")
-
-#print ('Khan received votes: %s' % khan_votes['Khan'])
-#print (khan_votes.most_common())
+#Add total votes
+total_votes = sum(candidate_votes.values())
 
 
+#Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, "w") as csvfile:
 
-#khan_result = Counter(khan_votes)
-#make a dictionary of candidates
-#electionresults = {
-    #'Khan': len(khan_votes),
-    #'Correy': len(correy_votes),
-    #"O'Tooley": len(tooley_votes),
-    #'Li': len(li_votes)}
-#decide winner
-#winner = (max(electionresults))
+    #Create empty list
+    voters_percentage = []
+    for i in candidate_votes:
+        voters_percentage = (float(candidate_votes[i]) / total_votes) * 100
 
-#print(len(khan_votes))
+        #round the percentage, add line breakers
+        election_data = (f"{i} {round(voters_percentage,0)}%  -  {candidate_votes[i]}\n")
 
+        print(election_data)
 
+        csvfile.write(election_data)
+     
 
+    #aggregate the data and choose a winner
+    for key in candidate_votes.keys():
+        if candidate_votes[key] == max(candidate_votes.values()):
+            winner = key
+  
+  #Export winners and total votes, Format data for output
+    print(">>>>---------------------------------<<<<")
+    print("   The Winner of the Election is:", winner)
+    print(">>>>---------------------------------<<<<")
+    csvfile.write(f"\n{'Winner: '}")
+    csvfile.write(winner)
 
-#average = float(total_votes / votecounter)
-#print("The average change is: ", "%.2f" % average)
+    csvfile.write(f"\n{'Total Votes: '}")
+    csvfile.write(f"{round(votecounter,0)}\n")   
 
-
-
-    
-# Specify the file to write to
-output_path = os.path.join("Output", "PyPoll_new.csv")
-# Preview Output
-PyPoll_new = os.path.join("Output", "PyPoll_new.csv")
-
-# Open the file using "write" mode. Specify the variable to hold the contents
-with open(PyPoll_new, 'w', newline='') as csvfile:
-
-    # Initialize csv.writer
-    csv_writer = csv.writer(csvfile, delimiter=',')
-
-    # Write the first row (column headers)
-    csv_writer.writerow(['Election Results'])
-
-    # Write the second row
-    csv_writer.writerow(['-----------------------------'])
-
-    # And so on:
-    csv_writer.writerow(['Total Votes:', votecounter, '100%'])
-    csv_writer.writerow(['-----------------------------'])
-    csv_writer.writerow(['Khan:', khan_votes, khan_percentage])
-    csv_writer.writerow(['Correy:', correy_votes, correy_percentage])
-    csv_writer.writerow(['Li:', li_votes, li_percentage])
-    csv_writer.writerow(['OTooley:', tooley_votes, tooley_percentage])
-    csv_writer.writerow(['-----------------------------'])
-    csv_writer.writerow(['Winner:', 'NAME', winner_percentage])
 
 
